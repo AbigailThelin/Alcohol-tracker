b1b0e1d89521eb649e23c75a8eefd3e71525764b
Abigail Thelin <abbyjthelin@gmail.com>
2018-04-13 16:15:20 -0600

    lagoon skeleton

--- src/components/Lagoon.tsx
+++ src/components/Lagoon.tsx
@@ -1,11 +1,55 @@
 import * as React from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
+import DatePicker from 'material-ui/DatePicker';
+import Paper from 'material-ui/Paper';
+import { IAlcohol } from '../interfaces/Interface';
+import Toggle from 'material-ui/Toggle';
 
-class Lagoon extends React.Component{
+
+import '../styles/alcohol.css'
+
+class Lagoon extends React.Component<IAlcohol, {
+        startDate: string;
+        endDate: string;
+}>{
+
+    constructor(props: any){
+        super(props);
+        this.state={
+            startDate: '',
+            endDate: ''
+        }
+    }
 
     render() {
         return(
-            <div>
-                Laggon ya
+            <div className="alcoholContainer">
+                <p className="alcoholTitle">Lagoon Tracker</p>
+                <Paper zDepth={3} className="paper">
+                    <div className="AlcoholPaper">
+                        <div className="leftSide">
+                            <TextField
+                                hintText="Name your event here"
+                                floatingLabelText="Name Of Event"
+                            />                   
+                            <TextField
+                                hintText="Tickets Allotted"
+                                floatingLabelText="How many tickets allotted"
+                            />
+                            <Toggle
+                                label="Vegetarian/Vegan"
+                            />
+                        </div>
+                        <div className="rightSide">
+                            <DatePicker hintText="Start Date" mode="landscape" />                    
+                            <DatePicker hintText="End Date" mode="landscape" />
+                        </div>
+                    </div>
+                    <div className="button">
+                        <RaisedButton className="finishButton" label="Create New Tracker" primary={true}/>
+                    </div>
+                </Paper>
             </div>
         )
     }
