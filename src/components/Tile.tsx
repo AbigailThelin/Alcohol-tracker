8864c8d8d60ab02b1c6e1951c508ffb7982d80a9
Abigail Thelin <abbyjthelin@gmail.com>
2018-04-16 10:47:12 -0600

    bit of styling on home

--- src/components/Tile.tsx
+++ src/components/Tile.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { ITile } from '../interfaces/Interface';
-import Paper from 'material-ui/Paper';
+// import Paper from 'material-ui/Paper';
 import { Link } from 'react-router-dom';
 
 import '../styles/tile.css';
@@ -10,11 +10,12 @@ class Tile extends React.Component<ITile> {
 
 
     render() {
-        console.log('welooooo', this.props.path)
         return(
-            <Paper id="Tile" zDepth={2} className="tileContainer">
-                <Link to={this.props.path}>{this.props.name}</Link>
-            </Paper>
+            <Link to={this.props.path} className="container">
+                <div id="Tile" className="card-1">
+                    <h3 className="tileTitle">{this.props.name} Tracker</h3>
+                </div>
+            </Link>
         );
     }
 }
