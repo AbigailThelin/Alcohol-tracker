fed8bd49c755eec34c9a181da0e9a87a447a53c3
Abigail Thelin <abbyjthelin@gmail.com>
2018-04-18 13:18:13 -0600

    form and filtering

--- src/components/Tracker.tsx
+++ src/components/Tracker.tsx
@@ -15,7 +15,8 @@ export default class Tracker extends React.Component<ITracker, any>{
             employees:[{name: 'joe', id: 12345},{name: 'kelly', id: 12321},{name: 'Jessica', id: 12255},{name: 'Paul', id: 98798}],
             ids:[],
             badgeID: '',
-            drinkModal: false
+            drinkModal: false,
+            currentEmployee: null
         }
     }
 
@@ -28,8 +29,21 @@ export default class Tracker extends React.Component<ITracker, any>{
     check = () => {
         const idsArray = this.state.ids;
         const badgeIDNumber = idsArray.concat(this.state.badgeID);
+        const allEmployees = this.state.employees
+        const Id = this.state.badgeID
+        
+        let filteredEmployee = allEmployees.filter(function(employee:any){
+            return employee.id === Number(Id)
+        })
 
-        this.setState({ids: badgeIDNumber, drinkModal: true})
+        this.setState({
+            ids: badgeIDNumber, 
+            drinkModal: true,
+            currentEmployee: filteredEmployee
+        })
+
+        document.getElementById
+
     }
 
     toggleModal = () => {
@@ -40,24 +54,30 @@ export default class Tracker extends React.Component<ITracker, any>{
         return(
             <div className="trackerContainer">
 
-            {!this.state.drinkModal ? null : <DrinkModal employees={this.state.employees} ids={this.state.ids} badgeID={this.state.badgeID} exit={this.toggleModal}/>}
+            {!this.state.drinkModal ? null : <DrinkModal ids={this.state.ids} badgeID={this.state.badgeID} exit={this.toggleModal} currentEmployee={this.state.currentEmployee}/>}
                
                 <Paper zDepth={2} className="trackingTile">
                 <div>
                     <h2>This.props.title</h2>
                     <p>Enter employees badge number if unable to scan</p>
                 </div>
+
+                <form className="form">
                     <TextField
                         // tslint:disable-next-line jsx-no-lambda
                         onChange={(e) => this.handleChange(e, 'badge')}
                         maxlength="5"
                         hintText="Badge Number"
+                        // type="text"
                     />
                     <RaisedButton
+                        className="submitBtn"
+                        type="reset"
                         primary={true}
                         label="Submit"
                         onClick={this.check}
                     />
+                </form>
                 </Paper>
             </div>
         )
--- src/components/drinkModal.tsx
+++ src/components/drinkModal.tsx
@@ -1,27 +1,55 @@
 import * as React from 'react';
 import '../styles/drinkModal.css';
 import { Card } from 'material-ui/Card';
+import RaisedButton from 'material-ui/RaisedButton';
 
 export interface IDrinkModal {
     exit: () => void;
     employees?: any;
     ids?: any;
     badgeID?: any;
+    currentEmployee: any;
 }
 
-export const DrinkModal:React.SFC<IDrinkModal> = ({ employees, ids, badgeID, exit}) => {
-    return(
-        <div className="drinkModalConatiner">
+class DrinkModal extends React.Component<IDrinkModal, any>{
 
-            <Card className="drinkModal">
-                {badgeID}
+    constructor(props: any){
+        super(props);
+        this.state={
+            twoDrinks: false
+        }
+    }
 
-                <div className="exitContainer">
-                    <i className="material-icons exit" onClick={exit}>clear</i>
-                </div>
-            </Card>
-        </div>
-    )
+    // componentDidMount(){
+        //get all employees here instead of passing through props
+    // }
+
+    render(){
+        const employeeName = this.props.currentEmployee[0].name;
+        return(
+            <div className="drinkModalConatiner">
+
+                <Card className="drinkModal">
+    
+                    <div className="exitContainer">
+                        <i className="material-icons exit" onClick={this.props.exit}>clear</i>
+                    </div>
+
+                    <div className="modalContent">
+                        <div className="text">
+                            <h2>{employeeName} has had 1 drink</h2>
+                        </div>
+
+                    <RaisedButton
+                        primary={true}
+                        label="Give a Drink"
+                    />
+                    </div>
+    
+                </Card>
+            </div>
+        )
+    }
 }
 
 export default DrinkModal;
